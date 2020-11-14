c68ccaa0f37c85a80c57223587c7dfed0a53ce35
Rohit <rohitpingale103@gmail.com>
2020-11-15 01:41:06 +0530

    added ginal GUI code

--- Filter_GUI.py
+++ Filter_GUI.py
@@ -19,9 +19,57 @@ class smoothingFiltes():
         trimmedMeanValue = np.mean(trimmedArray)/(arrayLength-2*trimGrayscale)
         return trimmedMeanValue
 
-    def inverseGradient():
-        pass
-    
+    def inverseGradient(self,array,P):
+        array=np.array(array, dtype=float)
+        final_image=[]
+        filter_array = array.flatten()
+        flattenarray = array.flatten()
+        #print(flattenarray)
+        value_of_p=P
+        center_pos=0
+        
+        if(len(flattenarray)%2==0):
+            center_pos=int(len(flattenarray)/2)
+        else:
+            center_pos=int((len(flattenarray)-1)/2)
+        #print(flattenarray.shape)
+        
+        #if(flattenarray[center_pos]==0):
+            #flattenarray=flattenarray+1
+        flattenarray[center_pos]=flattenarray[center_pos]+1  
+        for i in range(0,len(flattenarray)):
+            if(i==4):
+                continue
+            else:
+                #print("---------"+str(i)+"------"+str(flattenarray[i]))
+                #print(abs(flattenarray[4]-flattenarray[i]))
+                value=abs(flattenarray[i]-flattenarray[4])
+                if(value==0):
+                    value=value+1
+                else:
+                    continue
+                    
+                flattenarray[i]=1/value
+
+        #print(flattenarray)
+        flattenarray[center_pos]=0
+        #print(flattenarray)
+        sum_total=sum(flattenarray)
+
+        #print("SUm total is:"+str(sum_total))
+            
+        for i in range(0,len(flattenarray)):
+            if(i==4):
+                flattenarray[i]=value_of_p
+            else:
+                flattenarray[i]=((1-value_of_p)*(flattenarray[i]/sum_total))
+        #print(flattenarray)
+        Convolve_array=np.multiply(filter_array,flattenarray)
+        Gradient_Inverse_value=sum(Convolve_array)
+        #print(filter_array)
+        # print(Gradient_Inverse_value)
+        return Gradient_Inverse_value
+
     def imagePadding(self,band,paddingValue):
         ##vertical Top padding
         band = np.insert(band,0,band[0]*np.ones(paddingValue)[:,None],axis = 0)
@@ -33,43 +81,58 @@ class smoothingFiltes():
         band = np.insert(band, len(band[0]), band[:,-1]*np.ones(paddingValue)[:,None], axis=1)
         return band
     
-    def convolution(self,band):
+    def convolution(self,band,algo):
         if (self.k % 2 == 0):
             raise Exception("K should be odd number.")
             
         paddingValue = int((self.k - 1)/2)
         imageBand = self.imagePadding(band,paddingValue)
         arrayshape = imageBand.shape
-        outputband = np.zeros(shape=arrayshape)
-        
+        outputband = []
+
         for rowNo in range(arrayshape[0] - paddingValue):
+            outputbandCol = []
             for colNo in range(arrayshape[1] - paddingValue):
                 neighbourhoodMatrix = imageBand[rowNo:rowNo + self.k,colNo:colNo + self.k]
 #                 print(neighbourhoodMatrix)
-                trimmedValue = self.trimmedMean(neighbourhoodMatrix)
-                outputband[rowNo:rowNo + paddingValue,colNo:colNo + paddingValue] = trimmedValue
-        return outputband
+                if algo == 'Trimmed Mean Filter':
+                    avgValue = self.trimmedMean(neighbourhoodMatrix)
+                if algo == 'Inverse Gradient Filter':
+                    avgValue = self.inverseGradient(neighbourhoodMatrix)
+                outputbandCol.append(avgValue)
+            outputband.append(outputbandCol)
+        return np.array(outputband)
     
     def display(self,outputimage):
 #         plt.figure()
 #         plt.imshow(outputimage) 
 #         plt.show()
-        st.image(outputimage[0], caption=f"Processed image", use_column_width=True,)
+        final_image=cv2.merge((outputimage[2],outputimage[1],outputimage[0]))
+        cv2.imwrite('outimage.jpg',final_image)
+        cv2.imshow('Out Image', final_image)
+        st.image(final_image, caption=f"Output image", use_column_width=True)
         # cv2.imwrite(str('aniket1'+'.'+'jpg'),)
     
-    def trimmedSmoothing(self):
+    def trimmedSmoothing(self,algo=None):
         outputimage = []
         img = self.image
         for bandNumber in range(img.shape[2]):
             bandarray = np.array([array[:,bandNumber] for array in img])
-            outputband = self.convolution(bandarray) 
+            outputband = self.convolution(bandarray,algo) 
             outputimage.append(outputband)
         self.display(outputimage)
         return outputimage
 
-    def inverseGradientSmoothing(self,P):
-        pass
-        
+    def inverseGradientSmoothing(self,P,algo = None):
+        outputimage = []
+        img = self.image
+        for bandNumber in range(img.shape[2]):
+            bandarray = np.array([array[:,bandNumber] for array in img])
+            outputband = self.convolution(bandarray,P,algo) 
+            outputimage.append(outputband)
+        self.display(outputimage)
+        return outputimage
+
 
 Algorithm = st.sidebar.selectbox('Which algorithm do you want to implement?', ['Trimmed Mean Filter','Inverse Gradient Filter'])
 st.write("Image smoothing filter GUI")
@@ -90,10 +153,10 @@ if isinstance(inputimagefile,BytesIO):
     if Algorithm == 'Trimmed Mean Filter':
             K_trim = st.sidebar.slider('Select size of convolution matrix: n x n',3, 9)
             ourfilter = smoothingFiltes(img_array,K=K_trim)
-            outputimage = ourfilter.trimmedSmoothing()
+            outputimage = ourfilter.trimmedSmoothing(algo = 'Trimmed Mean Filter' )
 
     if Algorithm == 'Inverse Gradient Filter':
             K_inverse = st.sidebar.slider('Select size of convolution matrix: n x n ',3, 9,3)
             P = st.sidebar.slider('Select weight assigned to center pixel (p)',0.0, 1.0,0.5)
             ourfilter = smoothingFiltes(img_array,K=K_inverse)
-            outputimage = ourfilter.inverseGradientSmoothing(P)
+            outputimage = ourfilter.inverseGradientSmoothing(P,algo = 'Inverse Gradient Filter')
